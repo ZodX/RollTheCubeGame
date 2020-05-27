2546ae1d90f0a8c6766222985444b4621db183e4
Molnár András Imre <m.andras.imre00@gmail.com>
2020-05-27 13:07:45 +0200

    asd

--- create.sql
+++ create.sql
@@ -2176,3 +2176,14 @@ create sequence hibernate_sequence start with 1 increment by 1
         steps integer not null,
         primary key (id)
     )
+create sequence hibernate_sequence start with 1 increment by 1
+
+    create table GameResult (
+       id bigint not null,
+        created timestamp not null,
+        duration bigint not null,
+        player varchar(255) not null,
+        solved boolean not null,
+        steps integer not null,
+        primary key (id)
+    )
